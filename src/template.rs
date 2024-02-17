ac1cb81120064c43235061ddeaaaa979ee198bfb
Nathan Neff-Mallon <mallon.na@gmail.com>
2024-02-17 17:32:31 -0600

    add template.rs

---
+++ src/template.rs
@@ -0,0 +1,20 @@
+use crate::general_helpers::read_day_input_lines;
+
+const DAY: u8 = ;
+// problem at https://adventofcode.com/2022/day/
+
+pub(crate) fn part_1() {
+    if let Ok(lines) = read_day_input_lines(DAY) {
+        for line in lines.flatten() {
+        }
+    }
+    println!("Day {DAY} Part 1: incomplete");
+}
+
+pub(crate) fn part_2() {
+    if let Ok(lines) = read_day_input_lines(DAY) {
+        for line in lines.flatten() {
+        }
+    }
+    println!("Day {DAY} Part 2: incomplete");
+}
