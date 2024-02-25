30ce6fc1500fba1be8b499bf81a36120d84a0d90
Nathan Neff-Mallon <mallon.na@gmail.com>
2024-02-25 16:21:56 -0600

    Refactor day 17 to use Vec<u8> instead of Vec<Vec<bool>> for a 2x speedup

--- src/day_17.rs
+++ src/day_17.rs
@@ -1,37 +1,61 @@
 use std::cmp::{min, max};
-use std::collections::VecDeque;
+
 use crate::general_helpers::read_day_input_lines;
 
 const DAY: u8 = 17;
 // problem at https://adventofcode.com/2022/day/17
 
-fn generate_wide(top_of_rocks: usize) -> Vec<(usize, usize)>{
-    let x = top_of_rocks+4;
-    return vec![(x, 2), (x, 3), (x, 4), (x, 5)]
+#[derive(Clone)]
+struct Rock{
+    height_of_bottom: usize,
+    layers: Vec<u8>,
 }
 
-fn generate_plus(top_of_rocks: usize) -> Vec<(usize, usize)>{
-    let x = top_of_rocks+4;
-    return vec![(x, 3), (x+1, 2), (x+1, 3), (x+1, 4), (x+2, 3)]
+fn generate_wide(top_of_rocks: usize) -> Rock{
+    Rock{
+        height_of_bottom: top_of_rocks + 3,
+        layers: vec![0b0001_1110u8],
+    }
 }
 
-fn generate_L(top_of_rocks: usize) -> Vec<(usize, usize)>{
-    let x = top_of_rocks+4;
-    return vec![(x, 2), (x, 3), (x, 4), (x+1, 4), (x+2, 4)]
+fn generate_plus(top_of_rocks: usize) -> Rock{
+    Rock{
+        height_of_bottom: top_of_rocks + 3,
+        layers: vec![0b0000_1000u8,
+                     0b0001_1100u8,
+                     0b0000_1000u8],
+    }
 }
 
-fn generate_l(top_of_rocks: usize) -> Vec<(usize, usize)>{
-    let x = top_of_rocks+4;
-    return vec![(x, 2), (x+1, 2), (x+2, 2), (x+3, 2)]
+fn generate_L(top_of_rocks: usize) -> Rock{
+    Rock{
+        height_of_bottom: top_of_rocks + 3,
+        layers: vec![0b0001_1100u8,
+                     0b0000_0100u8,
+                     0b0000_0100u8],
+    }
 }
 
-fn generate_square(top_of_rocks: usize) -> Vec<(usize, usize)>{
-    let x = top_of_rocks+4;
-    return vec![(x, 2), (x, 3), (x+1, 2), (x+1, 3)]
+fn generate_l(top_of_rocks: usize) -> Rock{
+    Rock{
+        height_of_bottom: top_of_rocks + 3,
+        layers: vec![0b0001_0000u8,
+                     0b0001_0000u8,
+                     0b0001_0000u8,
+                     0b0001_0000u8],
+    }
 }
 
-fn generate_rock(top_of_rocks: usize, rock_count: usize) -> Vec<(usize, usize)>{
-    return match rock_count % 5{
+fn generate_square(top_of_rocks: usize) -> Rock{
+    Rock{
+        height_of_bottom: top_of_rocks + 3,
+        layers: vec![0b0001_1000u8,
+                     0b0001_1000u8],
+    }
+}
+
+fn generate_rock(top_of_rocks: usize, rock_count: usize) -> Rock{
+    match rock_count % 5{
         0 => generate_wide(top_of_rocks),
         1 => generate_plus(top_of_rocks),
         2 => generate_L(top_of_rocks),
@@ -41,87 +65,90 @@ fn generate_rock(top_of_rocks: usize, rock_count: usize) -> Vec<(usize, usize)>{
     }
 }
 
-fn slide_left(rock: &[(usize, usize)], chamber: &[Vec<bool>]) -> Vec<(usize, usize)>{
-    let mut new_rock = vec![];
-    for (x, y) in rock.into_iter(){
-        if chamber[*x][*y-1]{
-            return rock.to_vec().clone()
+fn slide_left(rock: &Rock, chamber: &[u8]) -> Rock{
+    let new_rock_layers = rock.layers.clone().into_iter().map(|r| r<<1).collect::<Vec<u8>>();
+    for (idx, layer) in new_rock_layers.iter().enumerate(){
+        if (chamber[rock.height_of_bottom + idx] & layer > 0) || (layer >= &0b1000_0000u8){
+            return rock.clone()
         }
-        new_rock.push((*x,*y-1));
     }
-    return new_rock
+    Rock{
+        height_of_bottom: rock.height_of_bottom,
+        layers: new_rock_layers,
+    }
 }
 
-fn slide_right(rock: &[(usize, usize)], chamber: &[Vec<bool>]) -> Vec<(usize, usize)>{
-    let mut new_rock = vec![];
-    for (x, y) in rock.into_iter(){
-        if chamber[*x][*y+1]{
-            return rock.to_vec().clone()
-        }
-        new_rock.push((*x,*y+1));
+fn slide_right(rock: &Rock, chamber: &[u8]) -> Rock{
+    for layer in rock.layers.iter(){
+        if layer % 2 == 1{return rock.clone()}
     }
-    return new_rock
+    let new_rock_layers = rock.layers.clone().into_iter().map(|r| r>>1).collect::<Vec<u8>>();
+    for (idx, layer) in new_rock_layers.iter().enumerate(){
+        if chamber[rock.height_of_bottom + idx] & layer > 0{
+            return rock.clone()
+        }
+    }
+    Rock{
+        height_of_bottom: rock.height_of_bottom,
+        layers: new_rock_layers,
+    }
 }
 
-fn slide_rock(rock: &[(usize, usize)], slide: char, chamber: &[Vec<bool>]) -> Vec<(usize,usize)>{
+fn slide_rock(rock: &Rock, slide: char, chamber: &[u8]) -> Rock{
     match slide{
-    '<' =>{
-        if min(rock[0].1, rock[1].1) == 0{
-            return rock.to_vec()
-        } else {
-            return slide_left(rock, chamber)
-        }
-    },
-    '>' =>{
-        if rock[3].1 == 6{
-            return rock.to_vec()
-        } else {
-            return slide_right(rock, chamber)
-        }
-    },
+    '<' => slide_left(rock, chamber),
+    '>' => slide_right(rock, chamber),
     _ => panic!("That's not a direction, friend")
     }
 }
 
-fn drop_rock(rock: &[(usize, usize)], chamber: &mut [Vec<bool>]) -> Option<Vec<(usize,usize)>>{
-    let mut new_rock = vec![];
-    for (x, y) in rock.into_iter(){
-        if chamber[*x-1][*y]{
+fn drop_rock(rock: &Rock, chamber: &mut [u8]) -> Option<Rock>{
+    let new_rock = Rock{
+        height_of_bottom: rock.height_of_bottom - 1,
+        layers: rock.layers.clone()
+    };
+    for (idx, layer) in new_rock.layers.iter().enumerate(){
+        if chamber[new_rock.height_of_bottom + idx] & layer > 0{
             fix_rock(rock, chamber);
             return None
         }
-        new_rock.push((x-1,*y));
     }
-    return Some(new_rock)
+    Some(new_rock)
 }
 
-fn fix_rock(rock: &[(usize, usize)], chamber: &mut [Vec<bool>]){
-    for (x, y) in rock.into_iter(){
-        if chamber[*x][*y]{println!("{rock:?}"); panic!("This rock already overlaps with another rock!")}
-        chamber[*x][*y] = true;
-    }
-}
-
-fn print_board(chamber: &[Vec<bool>], height: usize){
-    for row in (0..height+2).rev(){
-        println!("{row:>3} |{}|", chamber[row].clone().into_iter()
-            .map(|a| if a {'#'} else {'.'}).collect::<String>()
-        );
+fn fix_rock(rock: &Rock, chamber: &mut [u8]){
+    for (idx, layer) in rock.layers.iter().enumerate(){
+        if chamber[rock.height_of_bottom + idx] & layer > 0{
+            panic!("There should be no overlap");
+        }
+        chamber[rock.height_of_bottom + idx] += layer;
     }
 }
 
-fn print_full_row_numbers(chamber: &[Vec<bool>], height: usize){
-    for row in (0..height+2).rev(){
-        if chamber[row].clone().into_iter().map(|a| if a{1}else{0}).sum::<u32>() == 7{println!("{row:>3}")}
+fn print_board(chamber: &[u8], height: usize){
+    for row in (0..height+1).rev(){
+        if row == 0{println!("|-------|")} else{
+            let s = str::replace(&format!("|{:07b}|", chamber[row]),"0",".");
+            println!("{}",str::replace(&s,"1","#"));
+        }
+    }
+}
+
+fn print_full_row_numbers(chamber: &[u8], height: usize){
+    for row in (0..height+1).rev(){
+        if chamber[row] == 0b0111_1111u8 {println!("{row:>3}")}
             }
 }
 
 pub(crate) fn part_1() {
-    let mut chamber:Vec<Vec<bool>> = vec![vec![false;7];5257];
-    for i in 0..7{
-        chamber[0][i] = true;
-    }
-    let mut top_of_rocks = 0usize;
+    // The Vec<Vec<bool>> style takes 97.91 ms for 20,000 rocks and is >O(n)
+    // The Vec<u8> version takes 51.62 ms for 20,000 rocks and is closer to O(n)
+    // The Vec<u8> version takes 640 ms for 200,000 rocks
+    // That's 34 days for 1000000000000 rocks... there has to be some kind of repeating pattern
+    // answer is 30648
+    let mut chamber: Vec<u8> = vec![0u8;310000];
+    chamber[0] = 0b0111_1111u8;
+    let mut top_of_rocks = 1usize;
     let mut commands = vec!['a'];
     let mut command_idx = 0usize;
     if let Ok(lines) = read_day_input_lines(DAY) {
@@ -132,65 +159,43 @@ pub(crate) fn part_1() {
     } else {panic!("Some kind of error with reading the file")}
     let n_commands = commands.len();
     println!("n_commands: {n_commands}");
-    for rock_idx in 0..2022{
+
+    use std::time::Instant;
+    let now = Instant::now();
+    for rock_idx in 0..200000{
         let mut rock = generate_rock(top_of_rocks, rock_idx);
         loop {
-        rock = slide_rock(&rock, commands[command_idx], &chamber);
-        command_idx = (command_idx + 1) % n_commands;
-        match drop_rock(&rock, &mut chamber){
-            Some(r) => rock = r,
-            None => {top_of_rocks = max(rock[rock.len()-1].0, top_of_rocks); break},
-            };
+            rock = slide_rock(&rock, commands[command_idx], &chamber);
+            command_idx = (command_idx + 1) % n_commands;
+            match drop_rock(&rock, &mut chamber){
+                Some(r) => rock = r,
+                None => {top_of_rocks = max(rock.layers.len() + rock.height_of_bottom, top_of_rocks); break},
+                };
         }
     }
-    print_full_row_numbers(&chamber, top_of_rocks);
-    println!("Day {DAY} Part 1: {top_of_rocks}");
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+    //print_board(&chamber, top_of_rocks);
+    println!("Day {DAY} Part 1: {}",top_of_rocks-1);
 }
 
-fn check_if_truncatable(chamber: &[Vec<bool>], rock: &[(usize, usize)])->bool{
-
-    for row in rock.into_iter().map(|r| r.1){
-
-    }
-
-    return false
-}
-
-fn truncate_chamber(chamber: &mut VecDeque<Vec<bool>>, rock){
-
-}
-
-pub(crate) fn part_2() {
-    // Now the same thing, but one trillion blocks!
-    // I am going to start with the same strategy, only with truncating the tower.
-    // I'm going to see if truncating only on full rows is enough
-    let mut chamber: VecDeque<Vec<bool>>= VecDeque::from(vec![vec![false;7];5257]);
-    // I should make that a VecDeque of u8 instead! Wow!
-    for i in 0..7{
-        chamber[0][i] = true;
-    }
-    let mut top_of_rocks = 0usize;
-    let mut commands = vec!['a'];
-    let mut command_idx = 0usize;
-    if let Ok(lines) = read_day_input_lines(DAY) {
-        for line in lines.flatten() {
-            commands = line.trim().chars().collect::<Vec<char>>();
-            break
-        }
-    } else {panic!("Some kind of error with reading the file")}
-    let n_commands = commands.len();
-    println!("n_commands: {n_commands}");
-//     for rock_idx in 0..2022{
-//         let mut rock = generate_rock(top_of_rocks, rock_idx);
-//         loop {
-//         rock = slide_rock(&rock, commands[command_idx], &chamber);
-//         command_idx = (command_idx + 1) % n_commands;
-//         match drop_rock(&rock, &mut chamber){
-//             Some(r) => rock = r,
-//             None => {top_of_rocks = max(rock[rock.len()-1].0, top_of_rocks); break},
-//             };
-//         }
+// fn check_if_truncatable(chamber: &[u8], rock: &Rock)->bool{
+//
+//     for _row in rock.layers.iter().map(|r| r.1){
 //
 //     }
+//
+//     false
+// }
+
+// /// truncates the chamber, returns the total shift amount
+// fn truncate_chamber(chamber: &mut VecDeque<Vec<bool>>, rock: Rock) -> usize{
+//
+// }
+
+pub(crate) fn part_2() {
+    // Now the same thing, but one trillion blocks! There must be some kind of repeating pattern
+    // or it wouldn't be possible.
+
     println!("Day {DAY} Part 2: incomplete");
 }
