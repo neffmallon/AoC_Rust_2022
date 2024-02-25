fd3d744f437ff13d29f4d6e88970d34f95677e8b
Nathan Neff-Mallon <mallon.na@gmail.com>
2024-02-25 17:52:35 -0600

    Day 17 part 2

--- src/day_17.rs
+++ src/day_17.rs
@@ -103,6 +103,10 @@ fn slide_rock(rock: &Rock, slide: char, chamber: &[u8]) -> Rock{
 }
 
 fn drop_rock(rock: &Rock, chamber: &mut [u8]) -> Option<Rock>{
+    if rock.height_of_bottom == 0{
+        fix_rock(rock, chamber);
+        return None
+    }
     let new_rock = Rock{
         height_of_bottom: rock.height_of_bottom - 1,
         layers: rock.layers.clone()
@@ -127,11 +131,10 @@ fn fix_rock(rock: &Rock, chamber: &mut [u8]){
 
 fn print_board(chamber: &[u8], height: usize){
     for row in (0..height+1).rev(){
-        if row == 0{println!("|-------|")} else{
-            let s = str::replace(&format!("|{:07b}|", chamber[row]),"0",".");
-            println!("{}",str::replace(&s,"1","#"));
-        }
+        let s = str::replace(&format!("|{:07b}|", chamber[row]),"0",".");
+        println!("{}",str::replace(&s,"1","#"));
     }
+    println!("|-------|");
 }
 
 fn print_full_row_numbers(chamber: &[u8], height: usize){
@@ -143,12 +146,11 @@ fn print_full_row_numbers(chamber: &[u8], height: usize){
 pub(crate) fn part_1() {
     // The Vec<Vec<bool>> style takes 97.91 ms for 20,000 rocks and is >O(n)
     // The Vec<u8> version takes 51.62 ms for 20,000 rocks and is closer to O(n)
-    // The Vec<u8> version takes 640 ms for 200,000 rocks
+    // The Vec<u8> version takes 640 ms for 200,000 rocks.
     // That's 34 days for 1000000000000 rocks... there has to be some kind of repeating pattern
-    // answer is 30648
+    // cargo run --release takes 61 ms, but still 3.4 days for 1 trillion rocks
     let mut chamber: Vec<u8> = vec![0u8;310000];
-    chamber[0] = 0b0111_1111u8;
-    let mut top_of_rocks = 1usize;
+    let mut top_of_rocks = 0usize;
     let mut commands = vec!['a'];
     let mut command_idx = 0usize;
     if let Ok(lines) = read_day_input_lines(DAY) {
@@ -162,7 +164,7 @@ pub(crate) fn part_1() {
 
     use std::time::Instant;
     let now = Instant::now();
-    for rock_idx in 0..200000{
+    for rock_idx in 0..2022{
         let mut rock = generate_rock(top_of_rocks, rock_idx);
         loop {
             rock = slide_rock(&rock, commands[command_idx], &chamber);
@@ -176,26 +178,74 @@ pub(crate) fn part_1() {
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
     //print_board(&chamber, top_of_rocks);
-    println!("Day {DAY} Part 1: {}",top_of_rocks-1);
+    println!("Day {DAY} Part 1: {}",top_of_rocks);
 }
-
-// fn check_if_truncatable(chamber: &[u8], rock: &Rock)->bool{
-//
-//     for _row in rock.layers.iter().map(|r| r.1){
-//
-//     }
-//
-//     false
-// }
-
-// /// truncates the chamber, returns the total shift amount
-// fn truncate_chamber(chamber: &mut VecDeque<Vec<bool>>, rock: Rock) -> usize{
-//
-// }
 
 pub(crate) fn part_2() {
     // Now the same thing, but one trillion blocks! There must be some kind of repeating pattern
     // or it wouldn't be possible.
+    // for my input, there is a pattern of 2666 rows, about 1750 blocks. It starts between rows
+    // 403 and 778 (which repeats at 3268).
+    let mut chamber: Vec<u8> = vec![0u8;40000];
+    let mut top_of_rocks = 0usize;
+    let mut commands = vec!['a'];
+    let mut command_idx = 0usize;
+    if let Ok(lines) = read_day_input_lines(DAY) {
+        for line in lines.flatten() {
+            commands = line.trim().chars().collect::<Vec<char>>();
+            break
+        }
+    } else {panic!("Some kind of error with reading the file")}
+    let n_commands = commands.len();
+    println!("n_commands: {n_commands}");
 
-    println!("Day {DAY} Part 2: incomplete");
+
+    let check_depth = 50;
+    // rock_idx, command_idx, top_of_rocks, chamber from top_of_rocks 0 to top_of_rocks -10
+    let mut proposed_repeat = (0usize, 0usize, 0, vec![0u8;check_depth]);
+    let total_rocks = 1000000000000usize;
+    let mut n_repeats = 0usize;
+    let mut height_boost = 0usize;
+    let mut rock_idx = 0usize;
+    'outer: while rock_idx < total_rocks{
+        let mut rock = generate_rock(top_of_rocks, rock_idx);
+        loop {
+            // Get the repeat pattern. Once the repeat has been found, blast rock_idx to stratosphere
+            if (top_of_rocks >= 800) & (proposed_repeat.0 == 0){
+                proposed_repeat = (
+                    rock_idx,
+                    command_idx,
+                    top_of_rocks,
+                    (&chamber[top_of_rocks-check_depth..top_of_rocks+1]).to_vec()
+                )
+            } else if (n_repeats==0) & (top_of_rocks >= 3268) & (rock_idx % 5 == proposed_repeat.0%5) & (command_idx == proposed_repeat.1) {
+                let mut correct_count = 0;
+                for row in 0..check_depth+1{
+                    if chamber[top_of_rocks-check_depth + row] == proposed_repeat.3[row]{
+                        correct_count += 1;
+                    }
+                    if correct_count == check_depth+1{
+                        println!("Found a match! {}, {}, {}", proposed_repeat.0, proposed_repeat.1, proposed_repeat.2);
+                        println!("repeats at rock_idx: {rock_idx}, top_of_rocks {top_of_rocks}");
+                        let repeat_n_rocks = rock_idx - proposed_repeat.0;
+                        let repeat_n_rows = top_of_rocks - proposed_repeat.2;
+                        n_repeats = (total_rocks - rock_idx)/repeat_n_rocks;
+                        height_boost = repeat_n_rows * n_repeats;
+                        rock_idx += n_repeats * repeat_n_rocks
+                    }
+                }
+            }
+            rock = slide_rock(&rock, commands[command_idx], &chamber);
+            command_idx = (command_idx + 1) % n_commands;
+            match drop_rock(&rock, &mut chamber){
+                Some(r) => rock = r,
+                None => {
+                    top_of_rocks = max(rock.layers.len() + rock.height_of_bottom, top_of_rocks);
+                    rock_idx += 1;
+                    break
+                },
+            };
+        }
+    }
+    println!("Day {DAY} Part 2: {}", height_boost + top_of_rocks);
 }
